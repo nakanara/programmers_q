75b0b340f876aa62907c745fa9940e5861d6d317
nakanara <nakanara@naver.com>
2020-07-01 00:50:30 +0900

    read web

--- read_web/startjob.js
+++ read_web/startjob.js
@@ -29,10 +29,53 @@ console.log('Start ' +  filename);
 
   console.log('Go page=' + url);
 
+
   await page.goto(url);
-  await page.screenshot({ path: '/' + filename + ".png" });
-  await page.pdf({ path: '/' + filename +".pdf", format: "A4" });
+
+  let html = (await page.content());
+
+  if(isFile) {
+    fs.writeFile( '/'+filename+ '.html', html, 'utf-8', function(err) {
+      console.log('비동기적 파일 쓰기 완료');
+    });
+  }
+
+  await browser.close();
+
+  var $ = cheerio.load(html);
+
+  let title = $('title').text();
+  let companyInfo = $('#layerCompanyInfo').text();
+  // 전일
+  let prevPrice = $('p[data-realtime-prev-closing-price=yes]').text();
+  
+  // 고가
+  let highPrice = $('p[data-realtime-high-price=yes]').text();
+  let tradeVolume = $('p[data-realtime-acc-trade-volume=yes]').text();
+  let openingPrice = $('p[data-realtime-opening-price=yes]').text(); // 시가
+  let lowPrice = $('p[data-realtime-low-price=yes]').text(); // 저가
+  let tradePrice = $('p[data-realtime-acc-trade-price=yes]').text(); // 거래대금
+
+  let boxSummary = $('span.txtB');
+  let boxSummaryDD = $('dd', boxSummary).children();
+
+  console.log(boxSummaryDD.length);
+
+  let arrInfo = [];
+  $(boxSummaryDD).each((i, el) => {
+    let v = $(el).text();
+    console.log('i='+ i + '/' + v);
+    arrInfo.push(v);
+  })
+
+  console.log('title=' + title);
+  console.log('info=' + companyInfo);
+  console.log('전일=' + prevPrice);
+
+
+  // await page.screenshot({ path: '/' + filename + ".png" });
+  // await page.pdf({ path: '/' + filename +".pdf", format: "A4" });
 
   // 모든 스크래핑 작업을 마치고 브라우저 닫기
-  await browser.close();
+  
 })();
