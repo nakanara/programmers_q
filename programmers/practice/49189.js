b30c0fa4182d73bc0e0245fb8ef62c230e64dbfc
nakanara <nakanara.com@gmail.com>
2020-10-06 13:15:34 +0900

    10.06

--- programmers/practice/49189.js
+++ programmers/practice/49189.js
@@ -1,75 +1,86 @@
 'use strict';
 /*
-문제 설명
-셀수있는 수량의 순서있는 열거 또는 어떤 순서를 따르는 요소들의 모음을 튜플(tuple)이라고 합니다. n개의 요소를 가진 튜플을 n-튜플(n-tuple)이라고 하며, 다음과 같이 표현할 수 있습니다.
+많은 노드들을 의미합니다.
 
-(a1, a2, a3, ..., an)
-튜플은 다음과 같은 성질을 가지고 있습니다.
+노드의 개수 n, 간선에 대한 정보가 담긴 2차원 배열 vertex가 매개변수로 주어질 때, 1번 노드로부터 가장 멀리 떨어진 노드가 몇 개인지를 return 하도록 solution 함수를 작성해주세요.
 
-중복된 원소가 있을 수 있습니다. ex : (2, 3, 1, 2)
-원소에 정해진 순서가 있으며, 원소의 순서가 다르면 서로 다른 튜플입니다. ex : (1, 2, 3) ≠ (1, 3, 2)
-튜플의 원소 개수는 유한합니다.
-원소의 개수가 n개이고, 중복되는 원소가 없는 튜플 (a1, a2, a3, ..., an)이 주어질 때(단, a1, a2, ..., an은 자연수), 이는 다음과 같이 집합 기호 '{', '}'를 이용해 표현할 수 있습니다.
-
-{{a1}, {a1, a2}, {a1, a2, a3}, {a1, a2, a3, a4}, ... {a1, a2, a3, a4, ..., an}}
-예를 들어 튜플이 (2, 1, 3, 4)인 경우 이는
-
-{{2}, {2, 1}, {2, 1, 3}, {2, 1, 3, 4}}
-와 같이 표현할 수 있습니다. 이때, 집합은 원소의 순서가 바뀌어도 상관없으므로
-
-{{2}, {2, 1}, {2, 1, 3}, {2, 1, 3, 4}}
-{{2, 1, 3, 4}, {2}, {2, 1, 3}, {2, 1}}
-{{1, 2, 3}, {2, 1}, {1, 2, 4, 3}, {2}}
-는 모두 같은 튜플 (2, 1, 3, 4)를 나타냅니다.
-
-특정 튜플을 표현하는 집합이 담긴 문자열 s가 매개변수로 주어질 때, s가 표현하는 튜플을 배열에 담아 return 하도록 solution 함수를 완성해주세요.
-
-[제한사항]
-s의 길이는 5 이상 1,000,000 이하입니다.
-s는 숫자와 '{', '}', ',' 로만 이루어져 있습니다.
-숫자가 0으로 시작하는 경우는 없습니다.
-s는 항상 중복되는 원소가 없는 튜플을 올바르게 표현하고 있습니다.
-s가 표현하는 튜플의 원소는 1 이상 100,000 이하인 자연수입니다.
-return 하는 배열의 길이가 1 이상 500 이하인 경우만 입력으로 주어집니다.
-
-
-s	result
-"{{2},{2,1},{2,1,3},{2,1,3,4}}"	[2, 1, 3, 4]
-"{{1,2,3},{2,1},{1,2,4,3},{2}}"	[2, 1, 3, 4]
-"{{20,111},{111}}"	[111, 20]
-"{{123}}"	[123]
-"{{4,2,3},{3},{2,3,4,1},{2,3}}"	[3, 2, 4, 1]
-입출력 예에 대한 설명
-
+제한사항
+노드의 개수 n은 2 이상 20,000 이하입니다.
+간선은 양방향이며 총 1개 이상 50,000개 이하의 간선이 있습니다.
+vertex 배열 각 행 [a, b]는 a번 노드와 b번 노드 사이에 간선이 있다는 의미입니다.
+입출력 예
+n	vertex	return
+6	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]	3
+입출력 예 설명
+예제의 그래프를 표현하면 아래 그림과 같고, 1번 노드에서 가장 멀리 떨어진 노드는 4,5,6번 노드입니다.
 
 */
 
-function solution(s) {
-  var answer = [];
+function solution(n, edge) {
+  var answer = 0;
+ 
+  
+  let map = new Array(n+1);
 
-  let bb = JSON.parse(s.replace(/{/ig, '[').replace(/}/ig, ']'))
-    .sort((a,b) => {    
-      return a.length - b.length
-    });
+  for(let i=0; i <= n; i++) {
+    map[i] = new Array(n+1);
+   // map[i].fill(NaN, 0, n+1);
+  }
+  map[3][6] = true;
+  console.log(map);
+  
 
-  for(let v of bb) {
+  for (let i=0; i < edge.length; i++) {
+    let a = Number(edge[i][0]);
+    let b = Number(edge[i][1]);
+  
+    console.log(`${a} /// ${b}`);
+
+    map[a][b] = map[b][a] = true;    
+  }
+
+  function Queue(){
     
-    for(let j of v) {
+    let store = [];
 
-      let k = answer.findIndex(ai => ai === j);
-      if(k === -1) {
-        answer.push(j);
-        break;
-      }
-
+    function add(item) {
+      store.push(item);
     }
 
+    function get(item) {
+      return store.shift();
+    }
+
+    function toString(){
+      for(let v of store) {
+        console.log(v);
+      }
+    }
+
+    function isEmpty(){
+      return store.length === 0;
+    }
+
+    return {
+      add: add,
+      get: get,
+      toString: toString
+    };
+    
   }
-  
+
+  console.log('111');
+  let nodes = new Queue();
+
+  nodes.add(1);
+  nodes.toString();
+
+
+
   return answer;
+
+
 }
 
-console.log(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"));
-//console.log(solution("{{1,2,3},{2,1},{1,2,4,3},{2}}"));
-//console.log(solution("{{20,111},{111}}"));
-//console.log(solution("{{123}}"));
+
+solution(6,	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]);
