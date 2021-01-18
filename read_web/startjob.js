91e94b8de6a5b3a7f7f4d678499f62d9b1e95416
nakanara <nakanara.com@gmail.com>
2021-01-18 18:05:02 +0900

    merge

--- read_web/startjob.js
+++ read_web/startjob.js
@@ -1,14 +1,7 @@
 'use strict';
 
-<<<<<<< HEAD
-// let ReadWebPage = require('./web/ReadWebPage');
-let DBConnection = require('./db/DBConnection');
-let ReadWebPage = require('./web/ReadWebPage');
-let DBConnect = require('./db/DBConnect');
-=======
 const ReadWebPage = require('./web/ReadWebPage');
 const Sqls = require('./db/Sqls');
->>>>>>> b4fbc8482e84ccfcef2e11eed90ae672ee55cf4e
 
 var url = 'https://finance.daum.net/quotes/';
 var code = 'A086900';
@@ -21,19 +14,6 @@ var code = 'A086900';
 //  readWebPage.readFile('/F1593574328137.html');
 
 
-<<<<<<< HEAD
-// console.log('start');
-// const dbConnection = new DBConnection();
-// dbConnection.execute();
-// console.log('end');
-
-//DBConnect.asyncFunction();
-
-DBConnect.select("SELECT * FROM STOCK_COMP LIMIT 10", [], function(rows){
-
-  console.log('call');
-  console.log(rows);
-=======
 // DBConnect.asyncFunction();
 //sqlReader.loadSQL();
 // sqlReader.getSQL('STOCK_ITEM', 'update_stock_item');
@@ -60,15 +40,5 @@ sqls.select('STOCK_ITEM', 'get_stock_items', {STOCK_NAME: '동화약품'}, (data
 
   });
 
->>>>>>> b4fbc8482e84ccfcef2e11eed90ae672ee55cf4e
-
-
 });
 
-<<<<<<< HEAD
-// rows.forEach((r, i) => {
-//   console.log(r);
-// })
-=======
-
->>>>>>> b4fbc8482e84ccfcef2e11eed90ae672ee55cf4e
